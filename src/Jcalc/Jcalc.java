5740fc78cafe7215b19ab14182d09da49ba4c678
AlexeyGutniev <36887629+AlexeyGutniev@users.noreply.github.com>
2018-02-27 16:22:15 +0200

    added new line;

--- src/Jcalc/Jcalc.java
+++ src/Jcalc/Jcalc.java
@@ -3,6 +3,6 @@ package Jcalc;
 public class Jcalc {
     public static void main (String[]args){
         System.out.println("hi");
-        System.out.println("3");
+        System.out.println("33");
     }
 }
